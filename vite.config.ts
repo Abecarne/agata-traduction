da12e08abeeded131afa5ffacb6eba5f4e519b0d
Anthony Becarne <abecarne@MacBook-Pro-de-Anthony.local>
2025-04-11 22:44:36 +0200

    [+] New design from bolt

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [sveltekit()],
-	resolve: {
-		alias: {
-			$lib: '/src/lib',
-			$components: '/src/lib/components',
-		}
-	},
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
